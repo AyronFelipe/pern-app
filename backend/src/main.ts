7934df2e3072d09e6ef69e0a05167b5b40a6864f
Ayron Felipe <ayron41@gmail.com>
2023-11-22 17:36:13 -0300

    feat: adding cors lib

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,15 @@ import * as dotenv from 'dotenv'
 import express from 'express'
 import bodyParser from 'body-parser'
 import newsRouterV1 from './routes/v1/news.routes'
+import cors from 'cors'
 
 dotenv.config()
 
 async function bootstrap() {
   const app = express()
   const port = process.env.PORT || 3000
+
+  app.use(cors())
 
   app.use(bodyParser.json())
 
