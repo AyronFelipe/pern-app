692745b61b1c73781bbc4fa0fbfcfa1d7817ee03
Ayron Felipe <ayron41@gmail.com>
2023-11-23 01:25:10 -0300

    feat: breadcrumd

--- frontend/src/components/Detail/index.tsx
+++ frontend/src/components/Detail/index.tsx
@@ -1,9 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import DOMPurify from 'dompurify';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link as ReactRouterLink, useParams } from 'react-router-dom';
 
+import { ChevronRightIcon } from '@chakra-ui/icons';
 import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
   Card,
   CardBody,
   Center,
@@ -33,32 +37,52 @@ function Detail() {
   }, [newId, news]);
 
   return (
-    <Center paddingTop="4rem" paddingBottom="4rem">
-      <Card maxW="5xl" key={newInfo?.id}>
-        <CardBody>
-          <Center>
-            <Image src={newInfo?.imagem} borderRadius="lg" />
-          </Center>
-          <Stack mt={6} spacing={3}>
-            <Heading size="md">{newInfo?.titulo}</Heading>
-            <Flex justifyContent="end">
-              <Tag size="md" key="md">
-                {formatDateString(newInfo?.data_hora_publicacao)}
-              </Tag>
-            </Flex>
-            <Divider />
-            <Text
-              textAlign="justify"
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(
-                  newInfo?.conteudo ? newInfo?.conteudo : ''
-                ),
-              }}
-            />
-          </Stack>
-        </CardBody>
-      </Card>
-    </Center>
+    <>
+      <Breadcrumb
+        spacing="8px"
+        separator={<ChevronRightIcon color="gray.500" />}
+        paddingTop="1rem"
+        paddingLeft="4rem"
+      >
+        <BreadcrumbItem>
+          <BreadcrumbLink as={ReactRouterLink} to="/">
+            Início
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink as={ReactRouterLink} to="#">
+            {newInfo?.titulo}
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <Center paddingTop="4rem" paddingBottom="4rem">
+        <Card maxW="5xl" key={newInfo?.id}>
+          <CardBody>
+            <Center>
+              <Image src={newInfo?.imagem} borderRadius="lg" />
+            </Center>
+            <Stack mt={6} spacing={3}>
+              <Heading size="md">{newInfo?.titulo}</Heading>
+              <Flex justifyContent="end">
+                <Tag size="md" key="md">
+                  {formatDateString(newInfo?.data_hora_publicacao)}
+                </Tag>
+              </Flex>
+              <Divider />
+              <Text
+                textAlign="justify"
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(
+                    newInfo?.conteudo ? newInfo?.conteudo : ''
+                  ),
+                }}
+              />
+            </Stack>
+          </CardBody>
+        </Card>
+      </Center>
+    </>
   );
 }
 
